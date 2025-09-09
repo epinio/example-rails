385e5cb1a854109af9176524d6ce66a6f1cb43b2
David Johnson <dcharles525@gmail.com>
2025-09-09 17:11:11 -0400

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -23,7 +23,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile nil
-
+state_path nil
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
