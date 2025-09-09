6f769bbae44e3c449a8aa319f6f2deba8af903c3
David Johnson <dcharles525@gmail.com>
2025-09-09 16:27:06 -0400

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Ensure the tmp directory exists and set ownership
-mkdir -p tmp
-chown -R $USER:$USER tmp
-
-# Start the rails server
-bundle exec rails db:prepare && bundle exec puma -t 5:5 -p ${PORT:-3000} -e production
